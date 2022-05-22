c8eec173f4a194bd4816b26a23609b2f13a6c7ff
Sergei Khvalko <falko.serega2016@yandex.ru>
2022-05-22 18:49:57 +0300

    feat: create or update user

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,15 @@
 const admin = require("../firebase");
 
-exports.authCheck = (req, res, cb) => {
-  console.log(req.headers);
-  cb();
+exports.authCheck = async (req, res, cb) => {
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    req.user = firebaseUser;
+    cb();
+  } catch (err) {
+    res.status(401).json({
+      err: "Invalid or expired token",
+    });
+  }
 };
