1a87ca62d57bbad208c80a1cbdbecf6ec657cae6
Sergei Khvalko <falko.serega2016@yandex.ru>
2022-05-25 14:16:21 +0300

    feat: add category schema

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const admin = require("../firebase");
+const User = require("../models/user");
 
 exports.authCheck = async (req, res, cb) => {
   try {
@@ -11,5 +12,19 @@ exports.authCheck = async (req, res, cb) => {
     res.status(401).json({
       err: "Invalid or expired token",
     });
+  }
+};
+
+exports.adminCheck = async (req, res, cb) => {
+  const { email } = req.user;
+
+  const adminUser = await User.findOne({ email }).exec();
+
+  if (adminUser.role !== "admin") {
+    res.status(403).json({
+      err: "Admin resource. Access denied.",
+    });
+  } else {
+    cb();
   }
 };
