ee806547a6428ee1ef431d8ab0cbb9e29be179c9
sergei khvalko <falko.serega2016@yandex.ru>
2023-01-09 21:36:56 +0300

    feat: creating functions listAll and searchFilters in controller

--- controllers/product.js
+++ controllers/product.js
@@ -43,3 +43,176 @@ exports.list = async(req, res) => {
 		console.log(err);
 	}
 }
+
+exports.listAll = async(req, res) => {
+	try {
+		let products = await Product.find({})
+			.limit(parseInt(req.params.count))
+			.populate("category")
+			.populate("subs")
+			.sort([["createdAt", "desc"]])
+			.exec();
+		res.json(products);
+	} catch(err) {
+		console.log(err);
+	}
+}
+
+
+// SERACH / FILTER
+
+const handleQuery = async(req, res, query) => {
+	const products = await Product.find({ $text: { $search: query } })
+		.populate("category", "_id name")
+		.populate("subs", "_id name")
+		.populate("postedBy", "_id name")
+		.exec();
+
+	res.json(products);
+};
+
+const handlePrice = async (req, res, price) => {
+	try {
+		let products = await Product.find({
+			price: {
+				$gte: price[0],
+				$lte: price[1],
+			},
+		})
+			.populate("category", "_id name")
+			.populate("subs", "_id name")
+			.populate("postedBy", "_id name")
+			.exec();
+
+		res.json(products);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const handleCategory = async (req, res, category) => {
+	try {
+		let products = await Product.find({ category })
+			.populate("category", "_id name")
+			.populate("subs", "_id name")
+			.populate("postedBy", "_id name")
+			.exec();
+
+		res.json(products);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const handleStar = (req, res, stars) => {
+	Product.aggregate([
+		{
+			$project: {
+				document: "$$ROOT",
+				// title: "$title",
+				floorAverage: {
+					$floor: { $avg: "$ratings.star" }, // floor value of 3.33 will be 3
+				},
+			},
+		},
+		{ $match: { floorAverage: stars } },
+	])
+		.limit(12)
+		.exec((err, aggregates) => {
+			if (err) console.log("AGGREGATE ERROR", err);
+			Product.find({ _id: aggregates })
+				.populate("category", "_id name")
+				.populate("subs", "_id name")
+				.populate("postedBy", "_id name")
+				.exec((err, products) => {
+					if (err) console.log("PRODUCT AGGREGATE ERROR", err);
+					res.json(products);
+				});
+		});
+};
+
+const handleSub = async (req, res, sub) => {
+	const products = await Product.find({ subs: sub })
+		.populate("category", "_id name")
+		.populate("subs", "_id name")
+		.populate("postedBy", "_id name")
+		.exec();
+
+	res.json(products);
+};
+
+const handleShipping = async (req, res, shipping) => {
+	const products = await Product.find({ shipping })
+		.populate("category", "_id name")
+		.populate("subs", "_id name")
+		.populate("postedBy", "_id name")
+		.exec();
+
+	res.json(products);
+};
+
+const handleColor = async (req, res, color) => {
+	const products = await Product.find({ color })
+		.populate("category", "_id name")
+		.populate("subs", "_id name")
+		.populate("postedBy", "_id name")
+		.exec();
+
+	res.json(products);
+};
+
+const handleBrand = async (req, res, brand) => {
+	const products = await Product.find({ brand })
+		.populate("category", "_id name")
+		.populate("subs", "_id name")
+		.populate("postedBy", "_id name")
+		.exec();
+
+	res.json(products);
+};
+
+exports.searchFilters = async(req, res) => {
+	const {
+		query,
+		price,
+		category,
+		stars,
+		sub,
+		shipping,
+		color,
+		brand
+	} = req.body;
+
+	if(query) {
+		await handleQuery(req, res, query);
+	}
+
+	// price [20, 200]
+	if (price !== undefined) {
+		await handlePrice(req, res, price);
+	}
+
+	if (category) {
+		await handleCategory(req, res, category);
+	}
+
+	if (stars) {
+		await handleStar(req, res, stars);
+	}
+
+	if (sub) {
+		await handleSub(req, res, sub);
+	}
+
+	if (shipping) {
+		await handleShipping(req, res, shipping);
+	}
+
+	if (color) {
+		await handleColor(req, res, color);
+	}
+
+	if (brand) {
+		await handleBrand(req, res, brand);
+	}
+}
--- routes/product.js
+++ routes/product.js
@@ -5,8 +5,15 @@ const router = express.Router();
 const {authCheck, adminCheck} = require("../middleware/auth");
 
 // controller
-const {productsCount, list} = require("../controllers/product");
+const {productsCount, list, listAll, searchFilters} = require("../controllers/product");
 
 // routes
-router.get("/products/total", productsCount);
 router.post("/products", list);
+router.get("/products/total", productsCount);
+router.get("/products/:count", listAll); // products/100
+
+// search
+router.post("/search/filters", searchFilters);
+
+
+module.exports = router;
