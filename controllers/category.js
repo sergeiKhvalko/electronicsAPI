f0c3584d33157072b8a53ff550c60511e0000bab
sergei khvalko <falko.serega2016@yandex.ru>
2023-01-14 14:08:03 +0300

    feat: getSubs

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,6 @@
 const Category = require("../models/category");
+const Product = require("../models/product");
+const Sub = require("../models/sub");
 const slugify = require("slugify");
 
 exports.create = async (req, res) => {
@@ -17,7 +19,12 @@ exports.list = async (req, res) => {
 
 exports.read = async (req, res) => {
   const category = await Category.findOne({ slug: req.params.slug }).exec();
-  res.json(category);
+	const products = await Product.find({ category }).populate("category").exec();
+
+	res.json({
+		category,
+		products
+	});
 };
 
 exports.update = async (req, res) => {
@@ -34,6 +41,21 @@ exports.update = async (req, res) => {
   }
 };
 
+// DELETE CATEGORY AND DELETE IT'S CHILD SUB-CATEGORIES
+// exports.remove = async (req, res) => {
+//   try {
+//     const foundCat = await Category.findOne({ slug: req.params.slug });
+//     await Sub.deleteMany({ parent: { $in: foundCat._id } }, function (err) {
+//       console.log(err);
+//     });
+
+//     const deleted = await Category.findOneAndDelete({ slug: req.params.slug });
+//     res.json(deleted);
+//   } catch (err) {
+//     res.status(400).send("Category delete failed");
+//   }
+// };
+
 exports.remove = async (req, res) => {
   try {
     const deleted = await Category.findOneAndDelete({ slug: req.params.slug });
@@ -41,4 +63,11 @@ exports.remove = async (req, res) => {
   } catch (err) {
     res.status(400).send("Category delete failed");
   }
+};
+
+exports.getSubs = async (req, res) => {
+	Sub.find({ parent: req.params._id }).exec((err, subs) => {
+		if (err) console.log(err);
+		res.json(subs);
+	});
 };
