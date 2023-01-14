2a21468f9becb59333b9faa39236e72e576cec07
sergei khvalko <falko.serega2016@yandex.ru>
2023-01-14 17:37:45 +0300

    added functions read and update for product

--- routes/product.js
+++ routes/product.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { authCheck, adminCheck } = require("../middleware/auth");
 
 // controller
-const { create, productsCount, list, listAll, remove, searchFilters } = require("../controllers/product");
+const { create, productsCount, list, listAll, remove, searchFilters, read, update } = require("../controllers/product");
 
 // routes
 router.post("/product", authCheck, adminCheck, create)
@@ -13,6 +13,8 @@ router.post("/products", list);
 router.get("/products/total", productsCount);
 router.get("/products/:count", listAll); // products/100
 router.delete("/product/:slug", authCheck, adminCheck, remove);
+router.get("/product/:slug", read);
+router.put("/product/:slug", authCheck, adminCheck, update);
 
 // search
 router.post("/search/filters", searchFilters);
